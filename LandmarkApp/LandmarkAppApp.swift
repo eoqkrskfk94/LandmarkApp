ea7ec10011ea3469f3c1e4eefe694937d277dcb2
danielkim <37679062+eoqkrskfk94@users.noreply.github.com>
2022-01-19 20:05:08 +0900

    Initial Commit

---
+++ LandmarkApp/LandmarkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkAppApp.swift
+//  LandmarkApp
+//
+//  Created by 김명진 on 2022/01/19.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
